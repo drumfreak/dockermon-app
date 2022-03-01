e89272d9852ea9a673ab5520e2a17b2f06923e5d
ERock <webfreakeric@gmail.com>
2022-02-28 22:58:42 -0500

    update

--- frontend/docker-entrypoint-frontend.sh
+++ frontend/docker-entrypoint-frontend.sh
@@ -3,7 +3,7 @@ if [ ! -d "/app/dockermon-app/frontend/node_modules/.bin" ]
 then
     echo "Directory /app/dockermon-app/frontend/node_modules/ does not exist, installing node_modules. THIS WILL TAKE A FEW MINUTES ONLY THIS TIME"
     cd /app/dockermon-app/frontend
-    npm install --cache /app/tmp/npm/frontend_cache
+    npm install --production --cache /app/tmp/npm/frontend_cache
     rm -rf /app/tmp/npm/frontend_cache &
     npm run start
 else
